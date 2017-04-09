b13e84cccab1590cfa189004c76c9a95143f7934
Michaard <graniczkamichal@gmail.com>
2017-04-09 14:25:04 +0200

    Update

--- app/src/main/java/michaard/testapplication/Obrazek.java
+++ app/src/main/java/michaard/testapplication/Obrazek.java
@@ -1,12 +1,31 @@
 package michaard.testapplication;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 public class Obrazek extends Activity{
-    ImageView imageView=(ImageView)findViewById(R.id.imageView);
-    public void changePic(View v){
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.obrazek);
+    }
+    /*public void changePic(View v){
+        ImageView imageView=(ImageView)findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.android2);
+    }*/
+    public void changePicPrev(View v){
+        ImageView imageView=(ImageView)findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.android);
+    }
+    public void changePicNext(View v){
+        ImageView imageView=(ImageView)findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.android2);
+    }
+    public void back(View v){
+        Intent intent=new Intent(Obrazek.this,KlasaMain.class);
+        startActivity(intent);
     }
 }
